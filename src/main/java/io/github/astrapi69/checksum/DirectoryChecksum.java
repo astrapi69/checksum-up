afad5ead65e90f5c0834ad25df074abb2e595b39
astrapi69 <asterios.raptis@gmx.net>
2023-12-22 17:23:01 +0100

    prepare new major release in version 3
    - javadoc added

--- src/main/java/io/github/astrapi69/checksum/DirectoryChecksum.java
+++ src/main/java/io/github/astrapi69/checksum/DirectoryChecksum.java
@@ -29,10 +29,29 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
+/**
+ * The class {@link DirectoryChecksum} provides algorithms for computing checksum for directories
+ *
+ * @author Asterios Raptis
+ * @version 1.0
+ */
 public class DirectoryChecksum
 {
+
+	/**
+	 * The {@link MessageDigest} object
+	 */
 	MessageDigest messageDigest;
 
+	/**
+	 * Instantiates a new {@link DirectoryChecksum} object
+	 *
+	 * @param algorithm
+	 *            the algorithm for the {@link MessageDigest} object
+	 * @throws NoSuchAlgorithmException
+	 *             Is thrown if the algorithm is not supported or does not exists
+	 *             {@link MessageDigest} object
+	 */
 	DirectoryChecksum(String algorithm) throws NoSuchAlgorithmException
 	{
 		Objects.requireNonNull(algorithm, "Given algorithm is null");
@@ -40,6 +59,16 @@ public class DirectoryChecksum
 		this.messageDigest.reset();
 	}
 
+	/**
+	 * Updates the digest using the specified array of bytes and return the result as hexadecimal
+	 * {@link String} object
+	 *
+	 * @param dirPath
+	 *            the directory path
+	 * @return the hexadecimal {@link String} object
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred
+	 */
 	public String update(Path dirPath) throws IOException
 	{
 		Files.newDirectoryStream(dirPath).forEach(currentFile -> {
